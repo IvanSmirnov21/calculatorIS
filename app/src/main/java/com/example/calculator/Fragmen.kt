b70b19015597706c5bd9f851c608af022bd75cbd
Ivan Smirnov <89264148222@yandex.ru>
2020-11-03 16:35:29 +0300

    Фрагменты

--- app/src/main/java/com/example/calculator/Fragmen.kt
+++ app/src/main/java/com/example/calculator/Fragmen.kt
@@ -6,11 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class fragmen : Fragment() {
+class fragmenMain : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.activity_main, container, false)
     }
 
 }
